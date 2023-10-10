e253271270f71cae62c29b80c9f7ac87b5bed729
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-10-10 12:17:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+const word='madam'
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const characters=word.split('')
+  const reversedCharacters=characters.reverse()
+  const reversedWord=reversedCharacters.join('')
+  if(word.toLowerCase()===reversedWord.toLowerCase()){
+    return true
+
+  }
+  else{
+    return false
+  }
+  
 }
+console.log(isPalindrome(word))
 
 /* 
-  Add your pseudocode here
+  const characters=word.split('')
+  const reversedCharactes=characters.reverse()
+  const reversedWord=reversedCharacters.join('')
+  if word.toLowerCase===reversedWord.toLowerCase
+    return true
+  else
+    return false
+  define 
 */
 
 /*
   Add written explanation of your solution here
+  first convert the word to read backwards by
+use the split method to split the word into an array of characters
+use the reverse method to reverse the characters in the array
+use the join method to join the characters
+check if the word in lowercase is the same as the word converted backwards and in lowercase
 */
 
 // You can run `node index.js` to view these console logs
